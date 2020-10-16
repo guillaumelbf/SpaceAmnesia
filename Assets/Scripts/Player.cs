5aa5bb48e4bc9b7aa73978e1f1c99c3a8a6c0ce7
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2020-10-17 00:57:04 +0200

    Add player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    [SerializeField] float speed = 0;
+
+    private Vector2 curVeclocity = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = Vector2.zero;
+        verticalMove(Input.GetAxisRaw("Vertical"));
+        horizontalMove(Input.GetAxisRaw("Horizontal"));
+        rigidbody.velocity = rigidbody.velocity.normalized * speed;
+    }
+
+    void verticalMove(float _direction)
+    {
+        if(_direction.Equals(1))
+        {
+            rigidbody.velocity = Vector2.SmoothDamp(rigidbody.velocity, Vector2.up, ref curVeclocity,0.05f);
+        }
+        else if(_direction.Equals(-1))
+        {
+            rigidbody.velocity = Vector2.SmoothDamp(rigidbody.velocity, Vector2.down, ref curVeclocity, 0.05f);
+        }
+    }
+
+    void horizontalMove(float _direction)
+    {
+        if (_direction.Equals(1))
+        {
+            rigidbody.velocity = Vector2.SmoothDamp(rigidbody.velocity, Vector2.right, ref curVeclocity, 0.05f);
+        }
+        else if (_direction.Equals(-1))
+        {
+            rigidbody.velocity = Vector2.SmoothDamp(rigidbody.velocity, Vector2.left, ref curVeclocity, 0.05f);
+        }
+    }
+}
