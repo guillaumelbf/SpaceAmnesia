5fe561d4afe682289507af4ef7aa6b1556bcaba0
RaphaelChevasson <rc@clubcapelli.com>
2020-10-17 14:13:38 +0200

    Added sentiment analysis test scene and script

---
+++ Assets/Scripts/SentimentAnalysis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+/*
+using VaderSharp;
+*/
+public class SentimentAnalysis : MonoBehaviour
+{/*
+	public TMP_InputField inputField;
+
+	private SentimentIntensityAnalyzer analyzer;
+	
+
+	void Start()
+    {
+		analyzer = new SentimentIntensityAnalyzer();
+	}
+	
+    public void AnalyseSentiment()
+    {
+		print("Analysing text: " + inputField.text);
+		var results = analyzer.PolarityScores(inputField.text);
+		print("Positive score: " + results.Positive);
+		print("Negative score: " + results.Negative);
+		print("Neutral score: " + results.Neutral);
+		print("Compound score: " + results.Compound);
+	}*/
+}
