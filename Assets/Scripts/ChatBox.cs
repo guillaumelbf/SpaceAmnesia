80b3e1f83d1d0488624a53ed733d57cb5908e1f4
Antonin Galliot <a.galliot@student.isartdigital.com>
2020-10-17 09:11:01 +0200

    Locked player when writing, added talking box

--- Assets/Scripts/ChatBox.cs
+++ Assets/Scripts/ChatBox.cs
@@ -5,25 +5,54 @@ using UnityEngine.UI;
 
 public class ChatBox : MonoBehaviour
 {
+    [HideInInspector]
     public bool isWriting;
+    
+    [HideInInspector]
     public bool isTalking;
+    
+    [HideInInspector]
     public string msgChatBox;
-    public GameObject chatBox;
-    public GameObject chatBoxBox;
-    public TextMesh chatBoxTextPopUp;
+  
+    private GameObject _chatBox;
+    private GameObject _chatBoxBox;
+    private TextMesh   _chatBoxTextPopUp;
+
+    private SpriteRenderer _chatBoxBackground;
+    public Sprite thinkingBackgroundSprite;
+    public Sprite talkingBackgroundSprite;
 
     [SerializeField] public float maxTalkTime;
-    private float currTalkTime;
+    private float _currTalkTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        chatBox = transform.GetChild(0).gameObject;
-        chatBoxBox = transform.GetChild(0).GetChild(0).gameObject;
-        chatBoxTextPopUp = transform.GetChild(0).GetComponent<TextMesh>();
-        chatBox.SetActive(false);
         isWriting = false;
-        currTalkTime = maxTalkTime;
+        isTalking = false;
+        _currTalkTime = maxTalkTime;
+
+        /*
+         *  Setup chatbox components and objects in the script to avoid
+         *     catching them at each frame
+         */
+        
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.CompareTag("ChatBox"))
+            {
+                _chatBox = child.gameObject;
+                break;
+            }
+        }
+
+        if (_chatBox == null)
+            return;
+        _chatBox = transform.GetChild(0).gameObject;
+        _chatBoxBox = transform.GetChild(0).GetChild(0).gameObject;
+        _chatBoxBackground = _chatBoxBox.GetComponent<SpriteRenderer>();
+        _chatBoxTextPopUp = transform.GetChild(0).GetComponent<TextMesh>();
+        _chatBox.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,30 +60,35 @@ public class ChatBox : MonoBehaviour
     {
         if (!isWriting && !isTalking && Input.GetKeyDown(KeyCode.Return))
         {
-            chatBox.SetActive(true);
+            _chatBox.SetActive(true);
+            _chatBoxBackground.sprite = thinkingBackgroundSprite;
             msgChatBox = "";
             isWriting = true;
         }
         else if (isWriting && Input.GetKeyDown(KeyCode.Return))
         {
-            chatBox.SetActive(false);
+            _chatBoxBackground.sprite = talkingBackgroundSprite;
             Debug.Log(msgChatBox);
             isWriting = false;
             isTalking = true;
+            _currTalkTime = maxTalkTime;
         }
-        /*
         if (isTalking)
         {
-            currTalkTime -= Time.deltaTime;
-            if (currTalkTime <= 0.0f)
-                isTalking = false
-        }*/
+            _currTalkTime -= Time.deltaTime;
+            if (_currTalkTime <= 0.0f)
+            {
+                isTalking = false;
+                _chatBox.SetActive(false);
+
+            }
+        }
         if (isWriting)
         {
             msgChatBox += Input.inputString;
-            chatBoxBox.transform.localScale =
-                new Vector3((chatBoxTextPopUp.GetComponent<Renderer>().bounds.size.x + 1) * 75, 50, 1); 
-            chatBoxTextPopUp.text = msgChatBox;
+            _chatBoxBox.transform.localScale =
+                new Vector3((_chatBoxTextPopUp.GetComponent<Renderer>().bounds.size.x + 1) * 75, 50, 1); 
+            _chatBoxTextPopUp.text = msgChatBox;
             if (Input.GetKeyDown(KeyCode.Backspace) && msgChatBox.Length > 1)
                 msgChatBox = msgChatBox.Remove(msgChatBox.Length - 2);
         }
