408994c6e6899d974ab7de363d55cc2359a3d310
Antonin Galliot <a.galliot@student.isartdigital.com>
2020-10-17 19:29:26 +0200

    added trigger zone for dialogs

---
+++ Assets/Scripts/TriggerDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialog : MonoBehaviour
+{
+    public string dialogText;
+    public float timeBetweenLetters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetButton("Interact"))
+        {
+            string tempDialog = "";
+            tempDialog += dialogText;
+            tempDialog = tempDialog.Replace("\\n", "\n");
+            Dialog.PrintNewDialog(tempDialog, timeBetweenLetters, true);
+        }
+    }
+}
