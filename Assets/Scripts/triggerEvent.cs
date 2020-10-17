f8663f6cd7ea750935b58817ef6d0e7253f36f62
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2020-10-18 00:23:55 +0200

    Add sprite manager, and add sprite hover sprite

--- Assets/Scripts/triggerEvent.cs
+++ Assets/Scripts/triggerEvent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class triggerEvent : MonoBehaviour
+public class TriggerEvent : MonoBehaviour
 {
     [SerializeField] UnityEvent events = null;
     // Start is called before the first frame update
